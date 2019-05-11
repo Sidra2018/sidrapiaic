29afe3d4676eb51107a8e74d49d5d8cc1785f2d7
Sidra2018 <sidraeirfan@gmail.com>
2019-05-11 18:30:01 +0500

    Initial commit

---
+++ evenodd.py
@@ -0,0 +1,7 @@
+#check number is even or odd
+number=float (input("Enter your number")) #can use float instead of int
+if (number%2==0):
+    print("number is even")
+else:
+    print("number is odd")
+    
