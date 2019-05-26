4b4a987522250db090cdc7fa742867e787e2e2a6
Sidra2018 <sidraeirfan@gmail.com>
2019-05-26 13:46:40 +0500

    initialcommit

---
+++ loop.py
@@ -0,0 +1,18 @@
+num = int(input("Enter number : "))
+if num == 1 or num == 0:
+    print(num,"is not prime")
+elif num == 2:
+    print(num,"is prime")
+else:
+    iter = 2
+    for i in range(2, num):
+        iter +=1
+        if num % i ==0:
+            break
+    if iter == (num):
+    print(num,"is prime") 
+    else:
+    print(num,"is not primr")       
+    #print(num,"is not prime")
+
+
