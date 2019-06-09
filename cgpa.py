137f20765df5cbcc001d79df6e8d5fea4401bcc5
Sidra2018 <sidraeirfan@gmail.com>
2019-06-09 15:35:12 +0500

    This program calculates cgpa

---
+++ cgpa.py
@@ -0,0 +1,7 @@
+user_input = float(input(Enter your cgpa : ""))
+if user_input >= 3.7:
+    print("Your grade is A+")
+elif user_input < 3.7 and user_input >= 3.5:
+    print("Your grade is A")
+elif user_input < 3.5 and user_input >= 3.0:
+    print("Your grade is B")
